489f7c040cf5b0450ab7d0571e502f69905590b4
Brandon Berney <brandonberney@protonmail.com>
2015-10-05 13:42:31 -0400

    pygame sounds like a reasonable tool after today

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -1,10 +1,10 @@
 /**
  * A program to carry on conversations with a human user.
- * This is the initial version that:  
+ * This is the initial version that:
  * <ul><li>
  *       Uses indexOf to find strings
  * </li><li>
- * 		    Handles responding to simple words and phrases 
+ * 		    Handles responding to simple words and phrases
  * </li></ul>
  * This version uses a nested if to handle default responses.
  * @author Laurie White
@@ -13,17 +13,17 @@
 public class Magpie2
 {
 	/**
-	 * Get a default greeting 	
+	 * Get a default greeting
 	 * @return a greeting
 	 */
 	public String getGreeting()
 	{
 		return "Hello, let's talk.";
 	}
-	
+
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
@@ -42,6 +42,31 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}
+		else if (statement.indexOf("Mr. Pfeffer") >= 0)
+		{
+			response = "He sounds like a good teacher.";
+		}
+		else if (statement.trim().length() == 0)
+		{
+			response = "Say something, please.";
+		}
+		else if (statement.indexOf("movie") >= 0)
+		{
+			response = "Netflix and chill?";
+		}
+		else if (statement.indexOf("Mexico") >= 0)
+		{
+			response = "Donald Trump has many opinions on Mexico.";
+		}
+		else if (statement.indexOf("java") >= 0)
+		{
+			response = "Fun fact: This program was made in Java.";
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -55,11 +80,11 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
-		
+
 		if (whichResponse == 0)
 		{
 			response = "Interesting, tell me more.";
@@ -75,6 +100,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "That's hot.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "Yeah?";
 		}
 
 		return response;
